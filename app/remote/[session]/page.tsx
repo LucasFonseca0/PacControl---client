8545c4c0620ed13e3b5e8d0d90ae8c141a840e69
Lucas Fonseca <lucas.ribeiro.24444@gmail.com>
2024-11-23 22:20:50 +0000

    fix: fixed types

--- app/remote/[session]/page.tsx
+++ app/remote/[session]/page.tsx
@@ -18,7 +18,7 @@ export default function RemoteControl({
   const socketRef = useRef<Socket | null>(null)
   const joystickRef = useRef<HTMLDivElement>(null)
 
-  const serverURL:string = process.env.NEXT_PUBLIC_SERVER_URL!
+  const serverURL:any = process.env.NEXT_PUBLIC_SERVER_URL!
 
   useEffect(() => {
     const socket = io(serverURL, {
@@ -41,7 +41,7 @@ export default function RemoteControl({
       socket.disconnect()
     }
   }, [params.session, serverURL])
-
+ 
   useEffect(() => {
     if (joystickRef.current) {
       const joystick = nipplejs.create({
